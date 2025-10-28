c2c79d7f2c855ec7a8b097562cf2a12bdf82a0ec
Foxich55 <d.v.litvinoff@gmail.com>
2025-10-28 11:07:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ wnd = Tk()
 wnd.title('Отчёт успеваемости')
 wnd.geometry('700x700')
 
-btn_go = Button(text='Сравнить') #кнопка запуска сравнения
+def sravnit():
+    pass
+
+def choose():
+    pass
+
+btn_go = Button(text='Сравнить',command=sravnit) #кнопка запуска сравнения
 btn_go.pack()
 
-btn_c = Button(text='Выбрать файлы') #выбор файлов для сравнения
+btn_c = Button(text='Выбрать файлы',command=choose) #выбор файлов для сравнения
 btn_c.pack()
 
 
@@ -17,5 +23,7 @@ lbl_file1.pack()
 
 lbl_file2 = Label() #отображение файл2
 lbl_file2.pack()
+
+
 
 wnd.mainloop()
